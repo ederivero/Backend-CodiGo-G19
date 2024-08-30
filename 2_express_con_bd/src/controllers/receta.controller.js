9677abd590890ec87c8c92a30232933c94366223
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2024-08-29 22:16:49 -0500

    uso de prisma con express

--- 2_express_con_bd/src/controllers/receta.controller.js
+++ 2_express_con_bd/src/controllers/receta.controller.js
@@ -1,5 +1,17 @@
-export function crearReceta(req, res) {
+import { prisma } from "../cliente.js";
+
+export async function crearReceta(req, res) {
+  // Body del request
+  const body = req.body; // {nombre: 'Tres leches de vainilla' }
+  const resultado = await prisma.receta.create({
+    data: {
+      nombre: body.nombre,
+      descripcion: body.descripcion,
+    },
+  });
+
   return res.json({
     message: "Receta creada exitosamente",
+    content: resultado,
   });
 }
