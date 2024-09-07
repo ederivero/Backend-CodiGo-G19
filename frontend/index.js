28becf02f507ce833b71bd8bab911683faff0363
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2024-09-06 21:44:02 -0500

    frontend finalizado

--- frontend/index.js
+++ frontend/index.js
@@ -23,8 +23,8 @@ if (window.location.pathname.includes("crear-equipo.html")) {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          key: imagen.name,
-          //"path": "",
+          key: imagen.name.split(".")[0],
+          path: "equipos",
           contentType: imagen.type,
           extension: imagen.name.split(".").slice(-1)[0],
         }),
@@ -34,18 +34,49 @@ if (window.location.pathname.includes("crear-equipo.html")) {
     }
   };
 
+  const subirAlBucket = async (url) => {
+    const resultado = await fetch(url, { method: "PUT", body: imagen });
+    console.log(resultado.status);
+    return resultado.status === 200;
+  };
+
   imagenEquipo.addEventListener("change", (e) => {
     imagen = imagenEquipo.files[0];
     console.log(imagenEquipo.files[0]);
     generarUrlImagen()
       .then((r) => {
-        console.log(r);
+        return subirAlBucket(r.content);
+      })
+      .then((status) => {
+        if (!status) {
+          console.error("Error al subir la imagen");
+        }
       })
       .catch((e) => {
         console.log(e);
       });
   });
+
   btnCrearEquipo.addEventListener("click", (e) => {
     e.preventDefault();
+
+    fetch(`${BACKEND_URL}/equipo`, {
+      method: "POST",
+      body: JSON.stringify({
+        nombre: nombreEquipo.value,
+        imagen: {
+          key: imagen.name.split(".")[0],
+          path: "equipos",
+          contentType: imagen.type,
+          extension: imagen.name.split(".").slice(-1)[0],
+        },
+      }),
+    })
+      .then((r) => {
+        alert("EQUIPO CREADO EXITOSAMENTE");
+      })
+      .catch((e) => {
+        alert("ERROR AL CREAR EL EQUIPO");
+      });
   });
 }
