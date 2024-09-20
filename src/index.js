21e04c6e825b189dd495e7247d92297290fbad81
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2024-09-19 23:07:09 -0500

    uso de web socket

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import { enrutador } from "./router.js";
 import mongoose from "mongoose";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 const servidor = express();
+// Agregamos toda la funcionabilidad de express en nuestro servidor http
+const servidorHttp = createServer(servidor);
+const servidorSocket = new Server(servidorHttp, {
+  cors: {
+    origin: "*", // ["https://mifrontend.com", "http://mipaginafraudulenta.com"],
+  },
+});
 
 servidor.use(express.json());
 
@@ -15,9 +24,26 @@ const errorHandler = (error, req, res, next) => {
   });
 };
 
+// on sirve para indicar un evento que esperaremos que empiece el cliente para la conexion
+servidorSocket.on("connection", (cliente) => {
+  console.log(cliente.id);
+
+  // Emitimos un evento al cliente que se ha conectado
+  // Se va a emitir al cliente que se ha conectado
+  cliente.emit("saludo", "Buenas noches");
+
+  cliente.on("solicitar_hora_sistema", (argumentos) => {
+    console.log(argumentos);
+    // Tbn podemos emitir un evento a TOOOOODOS los clientes conectados
+    servidorSocket.emit("evento_de_la_hora_del_sistema", {
+      message: "ALGUIEN SOLICITO LA HORA",
+    });
+  });
+});
+
 servidor.use(errorHandler);
 
-servidor.listen(process.env.PORT, async () => {
+servidorHttp.listen(process.env.PORT, async () => {
   await mongoose.connect(process.env.MONGODB_URL); // mongodb://USUARIO:PASSWORD@HOST:PORT/DB
   console.log(
     `Servidor corriendo exitosamente en el puerto ${process.env.PORT}`
